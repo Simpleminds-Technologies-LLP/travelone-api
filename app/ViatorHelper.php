be038d0f892e3211893e87ae57f4246d9e652a08
Naresh Patel <30832955+nareshpatel007@users.noreply.github.com>
2023-10-16 08:49:57 +0530

    Update ViatorHelper.php

--- app/ViatorHelper.php
+++ app/ViatorHelper.php
@@ -135,6 +135,58 @@ class ViatorHelper
     }
 
     /**
+     * fetch booking questions
+     */
+    public static function fetch_product_booking_questions()
+    {
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL            => 'https://api.sandbox.viator.com/partner/products/booking-questions',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING       => '',
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_TIMEOUT        => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST  => 'GET',
+            CURLOPT_HTTPHEADER     => [
+                'exp-api-key: e1f06e53-937b-44c7-b392-b141ce1d0b91',
+                'Accept-Language: en-US',
+                'Accept: application/json;version=2.0'
+            ]
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return json_decode($response, true);
+    }
+
+    /**
+     * fetch product tags
+     */
+    public static function fetch_product_tags()
+    {
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL            => 'https://api.sandbox.viator.com/partner/products/tags',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING       => '',
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_TIMEOUT        => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST  => 'GET',
+            CURLOPT_HTTPHEADER     => [
+                'exp-api-key: e1f06e53-937b-44c7-b392-b141ce1d0b91',
+                'Accept-Language: en-US',
+                'Accept: application/json;version=2.0'
+            ]
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return json_decode($response, true);
+    }
+
+    /**
      * availability check
      */
     public static function availability_check($filter_data)
@@ -155,6 +207,71 @@ class ViatorHelper
                 'Accept: application/json;version=2.0',
                 'Content-Type: application/json'
             ),
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return json_decode($response, true);
+    }
+
+    /**
+     * fetch single location data
+     */
+    public static function viator_single_location_data($location_data)
+    {
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL            => 'https://api.sandbox.viator.com/partner/locations/bulk',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING       => '',
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_TIMEOUT        => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST  => 'POST',
+            CURLOPT_POSTFIELDS     => '{"locations": ["' . $location_data . '"]}',
+            CURLOPT_HTTPHEADER => array(
+                'exp-api-key: e1f06e53-937b-44c7-b392-b141ce1d0b91',
+                'Accept-Language: en-US',
+                'Accept: application/json;version=2.0',
+                'Content-Type: application/json',
+            ),
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return json_decode($response, true);
+    }
+
+    /**
+     * fetch single product reviews
+     */
+    public static function viator_single_product_reviews($product_code)
+    {
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL            => 'https://api.sandbox.viator.com/partner/reviews/product',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING       => '',
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_TIMEOUT        => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST  => 'POST',
+            CURLOPT_POSTFIELDS     => '{
+                "productCode": "' . $product_code . '",
+                "provider": "ALL",
+                "count": 500,
+                "start": 1,
+                "showMachineTranslated": true,
+                "reviewsForNonPrimaryLocale": true,
+                "ratings": [1,2,3,4,5],
+                "sortBy": "MOST_RECENT_PER_LOCALE"
+            }',
+            CURLOPT_HTTPHEADER => array(
+                'exp-api-key: e1f06e53-937b-44c7-b392-b141ce1d0b91',
+                'Accept-Language: en-US',
+                'Accept: application/json;version=2.0',
+                'Content-Type: application/json'
+            )
         ));
         $response = curl_exec($curl);
         curl_close($curl);
@@ -296,5 +413,192 @@ class ViatorHelper
 
         // return response
         return $return_arr;
+    }
+
+    /**
+     * filter booking questions
+     */
+    public static function filter_booking_questions($find_questions = [])
+    {
+        // define array
+        $return_arr = [];
+
+        // fetch product question list
+        $original_list = ViatorHelper::fetch_product_booking_questions();
+
+        // check is valid count
+        if(!empty($find_questions) && count($find_questions) && count($original_list['bookingQuestions'])) {
+            // fetch original question list
+            foreach ($original_list['bookingQuestions'] as $row_question) {
+                // match with original question
+                if(in_array($row_question['id'], $find_questions)) {
+                    // push data in array
+                    $return_arr[] = $row_question;
+                }
+            }
+        }
+
+        // return response
+        return $return_arr;
+    }
+
+    /**
+     * filter product tags
+     */
+    public static function filter_product_tags($find_tags = [])
+    {
+        // define array
+        $return_arr = [];
+
+        // fetch product product tags
+        $original_list = ViatorHelper::fetch_product_tags();
+
+        // check is valid count
+        if(!empty($find_tags) && count($find_tags) && count($original_list['tags'])) {
+            // fetch original question list
+            foreach ($original_list['tags'] as $row_tag) {
+                // match with original question
+                if(in_array($row_tag['tagId'], $find_tags)) {
+                    // push data in array
+                    $return_arr[] = [
+                        'tag_id'   => $row_tag['tagId'],
+                        'tag_name' => $row_tag['allNamesByLocale']['en'],
+                        'tag_slug' => ViatorHelper::str_slug($row_tag['allNamesByLocale']['en']),
+                    ];
+                }
+            }
+        }
+
+        // return response
+        return $return_arr;
+    }
+
+    /**
+     * filter product logistics
+     */
+    public static function filter_product_logistics($logistics = [])
+    {
+        // fetch start location
+        if(!empty($logistics['start']) && count($logistics['start'])) {
+            // fetch logistics start data
+            foreach ($logistics['start'] as $start_row_key => $start_row) {
+                // check ref location is set
+                if(!empty($start_row['location']['ref'])) {
+                    // find location data
+                    $location_list = ViatorHelper::viator_single_location_data($start_row['location']['ref']);
+
+                    // push data in array
+                    $logistics['start'][$start_row_key]['location'] = (count($location_list['locations'])) ? $location_list['locations'][0] : [];
+                }
+            }
+        }
+
+        // fetch end location
+        if(!empty($logistics['end']) && count($logistics['end'])) {
+            // fetch logistics end data
+            foreach ($logistics['end'] as $end_row_key => $end_row) {
+                // check ref location is set
+                if(!empty($end_row['location']['ref'])) {
+                    // find location data
+                    $location_list = ViatorHelper::viator_single_location_data($end_row['location']['ref']);
+
+                    // push data in array
+                    $logistics['end'][$end_row_key]['location'] = (count($location_list['locations'])) ? $location_list['locations'][0] : [];
+                }
+            }
+        }
+
+        // return response
+        return $logistics;
+    }
+
+    /**
+     * filter product itinerary
+     */
+    public static function filter_product_itinerary($itinerary_arr = [])
+    {
+        // fetch itinerary
+        if(!empty($itinerary_arr['itineraryItems']) && count($itinerary_arr['itineraryItems'])) {
+            // fetch logistics start data
+            foreach ($itinerary_arr['itineraryItems'] as $itinerary_key => $itinerary_row) {
+                // check ref location is set
+                if(!empty($itinerary_row['pointOfInterestLocation']['location']['ref'])) {
+                    // find location data
+                    $location_list = ViatorHelper::viator_single_location_data($itinerary_row['pointOfInterestLocation']['location']['ref']);
+
+                    // push data in array
+                    $itinerary_arr['itineraryItems'][$itinerary_key]['pointOfInterestLocation'] = $location_list['locations'][0];
+                }
+            }
+        }
+
+        // return response
+        return $itinerary_arr;
+    }
+
+    /**
+     * filter product itinerary
+     */
+    public static function filter_product_reviews($productCode)
+    {
+        // define array
+        $return_arr = [];
+
+        // fetch product reviews
+        $product_reviews = self::viator_single_product_reviews($productCode);
+
+        // count reviews
+        if(!empty($product_reviews['reviews']) && count($product_reviews['reviews'])) {
+            // fetch reviews
+            foreach ($product_reviews['reviews'] as $row) {
+                // push data in array
+                $return_arr[] = [
+                    'userName'      => $row['userName'],
+                    'title'         => $row['title'],
+                    'rating'        => $row['rating'],
+                    'text'          => $row['text'],
+                    'publishedDate' => $row['publishedDate'],
+                ];
+            }
+        }
+
+        // return response
+        return $return_arr;
+    }
+
+    /**
+     * filter product travelers photos
+     */
+    public static function filter_product_travelers_photos($filter_reiews = [])
+    {
+        // define array
+        $travelers_photos = [];
+
+        // count reviews
+        if(!empty($filter_reiews['reviews']) && count($filter_reiews['reviews'])) {
+            // fetch reviews
+            foreach ($filter_reiews['reviews'] as $row) {
+                // check photos info
+                if(!empty($row['photosInfo']) && count($row['photosInfo'])) {
+                    // fetch photos
+                    foreach ($row['photosInfo'] as $photo) {
+                        // count photo versions
+                        if(!empty($photo['photoVersions']) && count($photo['photoVersions'])) {
+                            // fetch photo versions
+                            foreach ($photo['photoVersions'] as $row_version) {
+                                // check photo size
+                                if($row_version['height'] == '733' && $row_version['width'] == '550') {
+                                    // push data in array
+                                    $travelers_photos[] = $row_version['url'];
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // return response
+        return $travelers_photos;
     }
 }
