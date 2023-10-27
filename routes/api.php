fbea53acd43b3591ff3b2823ce6feabd985958b5
Naresh Patel <30832955+nareshpatel007@users.noreply.github.com>
2023-10-27 08:15:57 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,3 +14,6 @@ Route::post('/viator/product_search', 'App\Http\Controllers\ViatorController@pro
 
 // product availability check
 Route::post('/viator/availability/check', 'App\Http\Controllers\ViatorController@availability_check')->name('availability_check');
+
+// bookings cart hold
+Route::post('/viator/bookings/cart_hold', 'App\Http\Controllers\ViatorController@bookings_cart_hold')->name('bookings_cart_hold');
