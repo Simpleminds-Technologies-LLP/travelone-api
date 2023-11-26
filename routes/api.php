079919b1e1f4db6b45c4395c675b0ad64413dd66
Naresh Patel <30832955+nareshpatel007@users.noreply.github.com>
2023-11-26 14:34:37 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,20 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
+// last updated products
+Route::post('/viator/last_updated_products', 'App\Http\Controllers\ViatorController@last_modified_since_product');
+
+// last updated availability schedules
+Route::post('/viator/last_updated_availability_schedules', 'App\Http\Controllers\ViatorController@last_modified_availability_schedules');
+
+// single product availability schedules
+Route::post('/viator/single_availability_schedules', 'App\Http\Controllers\ViatorController@single_product_availability_schedules');
+
 // product search
-Route::post('/viator/product_search', 'App\Http\Controllers\ViatorController@product_list')->name('viator_product_list');
+Route::post('/viator/product_search', 'App\Http\Controllers\ViatorController@fetch_product_list');
 
 // product availability check
-Route::post('/viator/availability/check', 'App\Http\Controllers\ViatorController@availability_check')->name('availability_check');
+Route::post('/viator/availability/check', 'App\Http\Controllers\ViatorController@availability_check');
 
 // bookings cart hold
-Route::post('/viator/bookings/cart_hold', 'App\Http\Controllers\ViatorController@bookings_cart_hold')->name('bookings_cart_hold');
+Route::post('/viator/bookings/cart_hold', 'App\Http\Controllers\ViatorController@bookings_cart_hold');
