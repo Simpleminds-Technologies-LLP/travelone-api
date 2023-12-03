d0d28bc78b362cc6fa4e9d0faf13701e28ca9f72
Naresh Patel <30832955+nareshpatel007@users.noreply.github.com>
2023-12-03 22:04:29 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -26,3 +26,8 @@ Route::post('/viator/availability/check', 'App\Http\Controllers\ViatorController
 
 // bookings cart hold
 Route::post('/viator/bookings/cart_hold', 'App\Http\Controllers\ViatorController@bookings_cart_hold');
+
+/**
+ * set cron job
+ */
+Route::get('/cron/update_activity_availability_schedules', 'App\Http\Controllers\ViatorController@update_activity_availability_schedules');
