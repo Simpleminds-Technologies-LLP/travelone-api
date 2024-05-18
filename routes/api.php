3bd76b5746891b74688f6a8b38197d4a0c2781c0
Naresh Patel <30832955+nareshpatel007@users.noreply.github.com>
2024-05-18 07:39:10 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -9,31 +9,31 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-// last updated products
+// Last updated products
 Route::post('/viator/last_updated_products', 'App\Http\Controllers\ViatorController@last_modified_since_product');
 
-// last updated availability schedules
+// Last updated availability schedules
 Route::post('/viator/last_updated_availability_schedules', 'App\Http\Controllers\ViatorController@last_modified_availability_schedules');
 
-// single product availability schedules
+// Single product availability schedules
 Route::post('/viator/single_availability_schedules', 'App\Http\Controllers\ViatorController@single_product_availability_schedules');
 
-// single product sync
+// Single product sync
 Route::post('/viator/single_product_search', 'App\Http\Controllers\ViatorController@single_fetch_product');
 
-// product availability check
+// Product availability check
 Route::post('/viator/availability/check', 'App\Http\Controllers\ViatorController@availability_check');
 
-// bookings cart hold
+// Bookings cart hold
 Route::post('/viator/bookings/cart_hold', 'App\Http\Controllers\ViatorController@bookings_cart_hold');
 
-// viator destinations
+// Viator destinations
 Route::post('/viator/destinations', 'App\Http\Controllers\ViatorController@viator_destinations');
 
-// set daily cron job
+// Set daily cron job
 Route::get('/cron/update_activity_availability_schedules', 'App\Http\Controllers\ViatorController@update_activity_availability_schedules');
 
-// product search
+// Sync viator products in TravelOne
 Route::get('/viator/sync/{country_id}/{start}', 'App\Http\Controllers\SyncController@sync_viator_product_list');
 
 
