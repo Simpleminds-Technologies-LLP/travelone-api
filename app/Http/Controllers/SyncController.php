db6ee7c2b4c3a0af17dde94b90e32091010d3e3f
Naresh Patel <30832955+nareshpatel007@users.noreply.github.com>
2024-07-08 23:22:53 +0530

    Update SyncController.php

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -780,7 +780,7 @@ class SyncController extends Controller
     public function sync_local_theme_from_tags(Request $request)
     {
         // Check if activity exists
-        $viator_product = DB::table('to_viator')->select('*')->where('status', 1)->where('theme_status', 0)->orderBy('id', 'ASC')->limit(100)->get();
+        $viator_product = DB::table('to_viator')->select('*')->where('status', 1)->where('theme_status', 0)->orderBy('id', 'ASC')->limit(300)->get();
 
         // Check is valid activity
         if(!empty($viator_product)) {
