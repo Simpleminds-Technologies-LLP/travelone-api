410ba396aaaf2493a8e86b5ba533ba420f0af306
Naresh Patel <30832955+nareshpatel007@users.noreply.github.com>
2024-08-01 08:33:10 +0530

    Update SyncController.php

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -15,6 +15,7 @@ class SyncController extends Controller
         $verify_tags_product_2 = DB::table('to_viator')->select('*')->where('tag_status', 2)->get();
         $verify_tags_product_3 = DB::table('to_viator')->select('*')->where('tag_status', 3)->get();
         $verify_review_product_2 = DB::table('to_viator')->select('*')->where('review_status', 2)->get();
+        $verify_attraction_product_2 = DB::table('to_viator')->select('*')->where('attraction_status', 2)->get();
 
         // Check is valid activity
         if(!empty($verify_main_product_2)) {
@@ -37,6 +38,25 @@ class SyncController extends Controller
         if(!empty($verify_review_product_2)) {
             // Fetch tours
             foreach ($verify_review_product_2 as $product) {
+                // Deactive product
+                DB::table('to_viator')->where('id', $product->id)->update([
+                    'status'              => 0,
+                    'availability_status' => 0,
+                    'tag_status'          => 0,
+                    'review_status'       => 0,
+                    'attraction_status'   => 0,
+                    'chatgpt_status'      => 0,
+                    'theme_status'        => 0,
+                    'review_count_status' => 0,
+                    'updated_at'          => NULL,
+                ]);
+            }
+        }
+
+        // Check is valid activity
+        if(!empty($verify_attraction_product_2)) {
+            // Fetch tours
+            foreach ($verify_attraction_product_2 as $product) {
                 // Deactive product
                 DB::table('to_viator')->where('id', $product->id)->update([
                     'status'              => 0,
