4c73a649d0304de0140b82f0a3faff1bb02bb689
Naresh Patel <30832955+nareshpatel007@users.noreply.github.com>
2024-08-07 07:53:08 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -34,7 +34,7 @@ Route::post('/viator/destinations', 'App\Http\Controllers\ViatorController@viato
 Route::get('/cron/update_activity_availability_schedules', 'App\Http\Controllers\ViatorController@update_activity_availability_schedules');
 
 // Sync viator products in TravelOne
-Route::get('/viator/sync/country_list', 'App\Http\Controllers\SyncController@sync_viator_product_list');
+Route::post('/viator/sync/country_list', 'App\Http\Controllers\SyncController@sync_viator_product_list');
 Route::get('/viator/sync/single_tour', 'App\Http\Controllers\SyncController@sync_viator_single_product');
 Route::get('/viator/sync/availability_schedules', 'App\Http\Controllers\SyncController@sync_viator_availability_schedules');
 Route::get('/viator/sync/categories_tag', 'App\Http\Controllers\SyncController@sync_viator_categories_tag');
